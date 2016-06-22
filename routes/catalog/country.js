87699db72e4c9a5837594e137d88ce8c70b8f733
altynbek.shaidyldaev@gmail.com <altynPc>
2016-06-22 13:21:18 +0600

    Config.json deleted from repo

--- routes/catalog/country.js
+++ routes/catalog/country.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res){
             if (err) return console.error(err);
 
             res.render('catalog/country', {
-                title: 'Регионы',
+                title: 'Страны',
                 catalog: catalog
             })
         });
