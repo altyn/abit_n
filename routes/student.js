f3968a9efcac84bc51f7a8dab98e358363c8e2cd
altynbek.shaidyldaev@gmail.com <altynPc>
2016-08-13 18:13:48 +0600

    Typeahead troubleshooting

--- routes/student.js
+++ routes/student.js
@@ -11,6 +11,12 @@ var Ort = require('../models/catalog/ort');
 var Country = require('../models/catalog/country');
 var Region = require('../models/catalog/region');
 var District = require('../models/catalog/district');
+var Groups = require('../models/catalog/groups');
+var Helpers = require('../lib/helpers');
+
+router.get('/', function(req, res){
+        res.render('student/students', { title: 'Студенты' });
+});
 
 router.get('/new', function(req, res){
 
@@ -20,7 +26,12 @@ router.get('/new', function(req, res){
     new_abiturient = {};
 
     async.parallel({
-
+        groups: function (callback) {
+            return Groups.find({}).sort('code').exec( function (err, result) {
+                if (err) console.error(err);
+                return callback(err, result);
+            });
+        },
         nationalities: function (callback) {
             return Nationality.find({}).sort('_id').exec( function (err, result) {
                 if (err) console.error(err);
@@ -109,8 +120,8 @@ router.post('/new', function(req, res, done){
 
     var newStud = new Student();
 
-    newStud.regnumber = idregnumber;
-    newStud.birthdate = parseDate(req.body.birthdate);
+    newStud._id = idregnumber;
+    newStud.birthdate = Helpers.parseDate(req.body.birthdate);
     newStud.lastname = req.body.surname;
     newStud.name = req.body.name;
     newStud.middlename = req.body.middle_name;
@@ -118,6 +129,7 @@ router.post('/new', function(req, res, done){
     newStud.citizenship = req.body.citizen;
     newStud.sex = req.body.sex;
     newStud.department = req.body.department;
+    newStud.group_code = req.body.group;
     newStud.formaobuch = req.body.formaobuch;
     newStud.acdegree = req.body.acdegree;
     newStud.to_course = req.body.course;
@@ -125,12 +137,12 @@ router.post('/new', function(req, res, done){
     newStud.pass_series = req.body.pass_series;
     newStud.pass_number = req.body.pass_number;
     newStud.pass_from = req.body.pass_from;
-    newStud.pass_date = parseDate(req.body.pass_date);
+    newStud.pass_date = Helpers.parseDate(req.body.pass_date);
 
     newStud.doc_type = req.body.d_type;
     newStud.doc_series = req.body.d_series;
     newStud.doc_number = req.body.d_number;
-    newStud.doc_date = parseDate(req.body.d_date);
+    newStud.doc_date = Helpers.parseDate(req.body.d_date);
 
     newStud.ortid = req.body.ort;
 
@@ -154,6 +166,7 @@ router.post('/new', function(req, res, done){
         if (err)
             console.error(err);
         req.flash('message', 'Данные успешно добавлены!');
+        console.log(newStud);
         res.redirect('/students');
     });
     //    }
@@ -161,6 +174,11 @@ router.post('/new', function(req, res, done){
 });
 
 router.get('/new/:regnumber', function(req, res){
+
+    //var query =  getComission(Comission, req.params.regnumber).exec(function(err,res){
+    //    if(err) console.log(err);
+    //    console.log(res);
+    //    });
 
     var regnumberId = req.params.regnumber;
 
@@ -170,20 +188,25 @@ router.get('/new/:regnumber', function(req, res){
     new_abiturient = {};
 
     async.parallel({
-
-        comission: function (callback) {
+        groups:         function (callback) {
+            return Groups.find({}).sort('code').exec( function (err, result) {
+                if (err) console.error(err);
+                return callback(err, result);
+            });
+        },
+        comission:      function (callback) {
             return Comission.find({regnumber : regnumberId }).sort('_id').exec( function (err, result) {
                 if (err) console.error(err);
                 return callback(err, result);
             });
         },
-        nationalities: function (callback) {
+        nationalities:  function (callback) {
             return Nationality.find({}).sort('_id').exec( function (err, result) {
                 if (err) console.error(err);
                 return callback(err, result);
             });
         },
-        departments: function (callback) {
+        departments:    function (callback) {
             return Department.find({}).sort('_id').exec( function (err, result) {
                 if(err) return(err);
                 if (!result){
@@ -194,7 +217,7 @@ router.get('/new/:regnumber', function(req, res){
                 }
             });
         },
-        formobuchs: function (callback) {
+        formobuchs:     function (callback) {
             return Formobuch.find({}).sort('_id').exec( function (err, result) {
                 if(err) return(err);
                 return callback(err, result)
@@ -206,19 +229,19 @@ router.get('/new/:regnumber', function(req, res){
                 return callback(err, result);
             });
         },
-        ort: function (callback) {
+        ort:            function (callback) {
             return Ort.find({}).sort({'created': -1}).exec( function (err, result) {
                 if(err) return(err);
                 return callback(err, result);
             });
         },
-        countries: function (callback) {
+        countries:      function (callback) {
             return Country.find({}).sort('_id').exec( function (err, result) {
                 if(err) return (err);
                 return callback(err, result);
             });
         },
-        regions: function (callback) {
+        regions:        function (callback) {
             return Region.find({}).sort('_id').exec( function (err, result) {
                 if(err) return(err);
                 if (!result){
@@ -230,7 +253,7 @@ router.get('/new/:regnumber', function(req, res){
 
             });
         },
-        districts: function (callback) {
+        districts:      function (callback) {
             return District.find({}).sort('_id').exec( function (err, result) {
                 if(err) return(err);
                 if (!result){
@@ -245,7 +268,7 @@ router.get('/new/:regnumber', function(req, res){
 
     }, function(err, new_abiturient){
         //res.jsonp(data)
-        res.render( 'student/student_new', {
+        res.render( 'student/student_from_comission', {
             title : 'Добавить нового студента',
             new_abiturient: new_abiturient
         });
@@ -254,4 +277,20 @@ router.get('/new/:regnumber', function(req, res){
 
 });
 
+router.get('/search.json', function(req, res) {
+    Comission.
+        find({}).
+        exec( function (err, data) {
+            if (err) console.error(err);
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify({ data: data }));
+
+        });
+});
+
 module.exports = router;
+
+function getComission (Model, regnumber) {
+    var query = Model.find({regnumber : regnumber});
+    return query;
+}
--- routes/teacher.js
+++ routes/teacher.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-    res.render('admin/teachers', { title: 'Преподаватели' });});
+    res.render('admin/teachers', { title: 'Преподаватели' });
+});
 
 module.exports = router;
